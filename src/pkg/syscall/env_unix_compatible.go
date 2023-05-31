f3fd0b5ce10fb85607987c6a3065bcc9756c5098
general <generals.space@gmail.com>
2023-05-31 21:12:07 +0800

    补充: 测试用例

--- src/pkg/syscall/env_unix_compatible.go
+++ src/pkg/syscall/env_unix_compatible.go
@@ -1,6 +1,8 @@
 package syscall
 
-// 暂时不考虑 cgo 的场景.
+// 	@todo 开启 cgo 的场景, 见
+// [golang 1.4 os.Unsetenv()](https://github.com/golang/go/blob/go1.4/src/syscall/env_unix.go#L34)
+//
 // func unsetenv_c(k string)
 
 // 	@compatible: 该函数在 v1.4 版本初次添加
