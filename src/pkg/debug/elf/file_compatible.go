b6d15ceb53ca9a48a44763d8d4115312ffeaaac8
general <generals.space@gmail.com>
2023-06-26 00:57:21 +0800

    补充

--- src/pkg/debug/elf/file_compatible.go
+++ src/pkg/debug/elf/file_compatible.go
@@ -1,5 +1,7 @@
 package elf
 
+import "errors"
+
 // 	@compatible: 此函数在 v1.3 版本初次添加
 //
 // DynamicSymbols returns the dynamic symbol table for f.
@@ -12,3 +14,9 @@ func (f *File) DynamicSymbols() ([]Symbol, error) {
 	sym, _, err := f.getSymbols(SHT_DYNSYM)
 	return sym, err
 }
+
+// 	@compatible: 此错误在 v1.3 版本初次添加
+//
+// ErrNoSymbols is returned by File.Symbols and File.DynamicSymbols
+// if there is no such section in the File.
+var ErrNoSymbols = errors.New("no symbol section")
