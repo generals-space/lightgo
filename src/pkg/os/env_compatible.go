4000e9ee5aeb29e6081b91a8761e80cbbb91a0f6
general <generals.space@gmail.com>
2023-06-27 21:48:30 +0800

    新增: os.LookupEnv()函数, 包含对应的测试示例

--- src/pkg/os/env_compatible.go
+++ src/pkg/os/env_compatible.go
@@ -8,3 +8,15 @@ import "syscall"
 func Unsetenv(key string) error {
 	return syscall.Unsetenv(key)
 }
+
+// 	@compatible: 该函数在 v1.5 版本初次添加
+//
+// 	@note: ...跟 Getenv() 是相同的功能, 重复了吧?
+//
+// LookupEnv retrieves the value of the environment variable named by the key.
+// If the variable is present in the environment the value (which may be empty)
+// is returned and the boolean is true.
+// Otherwise the returned value will be empty and the boolean will be false.
+func LookupEnv(key string) (string, bool) {
+	return syscall.Getenv(key)
+}
