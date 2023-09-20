7f2f8098efbb36c26899e4125d1304933b21e065
general <generals.space@gmail.com>
2023-09-21 06:51:42 +0800

    更新: PATH环境变量

--- init.sh
+++ init.sh
@@ -17,12 +17,12 @@ export GO_PATH_EXTENSION=/usr/local/gopath_extension
 ## 兼容标准库
 export GO_PATH_COMPATIBLE=/usr/local/go/compatible
 
-export GOPATH=/usr/local/gopath:${GO_PATH_LOCAL}:${GO_PATH_EXTENSION}:${GO_PATH_COMPATIBLE}:/usr/local/bison/bin
+export GOPATH=/usr/local/gopath:${GO_PATH_LOCAL}:${GO_PATH_EXTENSION}:${GO_PATH_COMPATIBLE}
 
 ## 将 dist, 6g, 6l 等命令加入 PATH
 export PATH=$PATH:${GOROOT}/bin:${GO_TOOL}:${GO_TOOLLIST}
 ## 将各 GOPATH 下的 bin 目录加入 PATH
-export PATH=$PATH:/usr/local/gopath/bin:${GO_PATH_LOCAL}/bin:${GO_PATH_EXTENSION}/bin
+export PATH=$PATH:/usr/local/gopath/bin:${GO_PATH_LOCAL}/bin:${GO_PATH_EXTENSION}/bin:/usr/local/bison/bin
 
 function gb() {
     go build -gcflags='-l -N' -o main.gobin $@
